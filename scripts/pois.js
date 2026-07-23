8b6868b8ec797ce2cb44268d7ea3283197dbd901
ImNotYourSpiirit <jules2004.restelli@gmail.com>
2026-07-23 13:17:52 -0500

    Update pois.js

--- scripts/pois.js
+++ scripts/pois.js
@@ -1,1 +1,95 @@
-//
+let pois = [];
+let poiMarkers = [];
+
+async function loadPOIs() {
+
+    const response = await fetch("data/pois.json");
+
+    pois = await response.json();
+
+}
+
+function getPOIIcon(type){
+
+    switch(type){
+
+        case "fuel":
+            return "⛽";
+
+        case "hotel":
+            return "🏨";
+
+        case "restaurant":
+            return "🍔";
+
+        case "landmark":
+            return "🏛";
+
+        default:
+            return "📍";
+
+    }
+
+}
+
+function drawPOIs(){
+
+    poiMarkers.forEach(marker=>map.removeLayer(marker));
+
+    poiMarkers=[];
+
+    pois.forEach(poi=>{
+
+        if(!poi.discovered) return;
+
+        const marker=L.marker([poi.lat,poi.lng]);
+
+        marker.bindPopup(
+
+            `<b>${getPOIIcon(poi.type)} ${poi.name}</b>`
+
+        );
+
+        marker.addTo(map);
+
+        poiMarkers.push(marker);
+
+    });
+
+}
+
+function discoverPOIs(){
+
+    pois.forEach(poi=>{
+
+        if(poi.discovered) return;
+
+        const d=map.distance(
+
+            playerMarker.getLatLng(),
+
+            [poi.lat,poi.lng]
+
+        );
+
+        if(d<150){
+
+            poi.discovered=true;
+
+            drawPOIs();
+
+            showMessage(
+
+                getPOIIcon(poi.type)
+
+                +" Découverte : "
+
+                +poi.name
+
+            );
+
+        }
+
+    });
+
+}
