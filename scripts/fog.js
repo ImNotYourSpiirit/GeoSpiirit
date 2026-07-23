a0cce78f316f2de71dc066358ce3fa1426de0ed3
ImNotYourSpiirit <jules2004.restelli@gmail.com>
2026-07-23 12:53:26 -0500

    Update fog.js

--- scripts/fog.js
+++ scripts/fog.js
@@ -1,1 +1,32 @@
-//
+const fog = {
+
+    tileSize: 0.25,
+
+    discovered: {},
+
+    layers: {}
+
+};
+
+function getTileKey(lat, lng) {
+
+    const x = Math.floor(lng / fog.tileSize);
+    const y = Math.floor(lat / fog.tileSize);
+
+    return `${x}_${y}`;
+
+}
+
+function discoverTile(lat, lng) {
+
+    const key = getTileKey(lat, lng);
+
+    fog.discovered[key] = true;
+
+}
+
+function isTileDiscovered(lat, lng) {
+
+    return fog.discovered[getTileKey(lat, lng)] === true;
+
+}
