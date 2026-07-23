3b2ec352260660dcd8fa10139ad876b066eb882e
ImNotYourSpiirit <jules2004.restelli@gmail.com>
2026-07-23 13:59:27 -0500

    Update memoryManager.js

--- scripts/memoryManager.js
+++ scripts/memoryManager.js
@@ -1,1 +1,72 @@
-//
+// ======================================
+// GeoSpiirit - Memory Manager
+// Gestion de la mémoire
+// ======================================
+
+
+
+const MAX_MEMORY_CELLS = 20;
+
+
+
+function unloadUnusedCells(requiredCells){
+
+
+
+    const cells = Object.keys(loadedCells);
+
+
+
+    if(cells.length <= MAX_MEMORY_CELLS){
+
+        return;
+
+    }
+
+
+
+    cells.forEach(cell=>{
+
+
+
+        if(!requiredCells.includes(cell)){
+
+
+            delete loadedCells[cell];
+
+
+            console.log(
+                "🗑️ Cellule supprimée :",
+                cell
+            );
+
+
+        }
+
+
+    });
+
+
+}
+
+
+
+// Retourne l'état mémoire
+
+function getMemoryUsage(){
+
+
+    return {
+
+        loadedCells:
+        Object.keys(loadedCells).length,
+
+
+        limit:
+        MAX_MEMORY_CELLS
+
+
+    };
+
+
+}
