f59763108e16c4c1abc1939fd9fd36816dcb8db2
ImNotYourSpiirit <jules2004.restelli@gmail.com>
2026-07-23 12:49:59 -0500

    Create vehicles.js

---
+++ scripts/vehicles.js
@@ -0,0 +1,38 @@
+const vehicles = {
+
+    foot: {
+        name: "On Foot",
+        speed: 1,
+        fuelConsumption: 0,
+        storage: 5
+    },
+
+    bike: {
+        name: "Bike",
+        speed: 2,
+        fuelConsumption: 0,
+        storage: 10
+    },
+
+    car: {
+        name: "Car",
+        speed: 5,
+        fuelConsumption: 0.08,
+        storage: 40
+    },
+
+    suv: {
+        name: "SUV",
+        speed: 4,
+        fuelConsumption: 0.11,
+        storage: 60
+    },
+
+    camper: {
+        name: "Camper",
+        speed: 3,
+        fuelConsumption: 0.15,
+        storage: 120
+    }
+
+};
