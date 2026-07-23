883ca4d9adefffb49df90fe4c9b2ce08a8a40742
ImNotYourSpiirit <jules2004.restelli@gmail.com>
2026-07-23 12:48:16 -0500

    Update player.js

--- scripts/player.js
+++ scripts/player.js
@@ -1,1 +1,28 @@
-//
+const player = {
+
+    name: "Traveler",
+
+    lat: -33.8688,
+    lng: 151.2093,
+
+    money: 500,
+
+    fuel: 60,
+    maxFuel: 60,
+
+    food: 100,
+    water: 100,
+    energy: 100,
+
+    xp: 0,
+    level: 1,
+
+    vehicle: "car",
+
+    discoveredCities: [],
+
+    journal: [],
+
+    inventory: []
+
+};
