cc9f787aad5db7df41072986cbdcd4e0a7d1222f
ImNotYourSpiirit <jules2004.restelli@gmail.com>
2026-07-23 12:53:43 -0500

    Update save.js

--- scripts/save.js
+++ scripts/save.js
@@ -1,1 +1,43 @@
-//
+function saveGame(){
+
+localStorage.setItem(
+
+"GeoSpiirit",
+
+JSON.stringify({
+
+player,
+
+fog,
+
+cities
+
+})
+
+);
+
+}
+
+function loadGame(){
+
+const save=
+
+localStorage.getItem("GeoSpiirit");
+
+if(!save) return;
+
+const data=JSON.parse(save);
+
+Object.assign(player,data.player);
+
+Object.assign(fog,data.fog);
+
+data.cities.forEach((city,i)=>{
+
+cities[i].discovered=city.discovered;
+
+});
+
+}
+
+setInterval(saveGame,30000);
