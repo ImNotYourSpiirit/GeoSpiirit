41f0c75e87682e7ab85662cfde617ee8346104de
ImNotYourSpiirit <jules2004.restelli@gmail.com>
2026-07-23 12:50:47 -0500

    Update ui.js

--- scripts/ui.js
+++ scripts/ui.js
@@ -1,1 +1,35 @@
-//
+const hud=document.getElementById("hud");
+
+function updateHUD(){
+
+hud.innerHTML=`
+
+💰 ${player.money}$
+
+<br>
+
+⛽ ${player.fuel.toFixed(1)} L
+
+<br>
+
+🍞 ${player.food}
+
+<br>
+
+💧 ${player.water}
+
+<br>
+
+⚡ ${player.energy}
+
+<br>
+
+⭐ Niveau ${player.level}
+
+<br>
+
+XP : ${player.xp}
+
+`;
+
+}
