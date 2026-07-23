a3a991867167a84c599d98be958adec7c10eb1ca
ImNotYourSpiirit <jules2004.restelli@gmail.com>
2026-07-23 15:55:22 -0500

    Create cellRenderer.js

---
+++ scripts/cellRenderer.js
@@ -0,0 +1,165 @@
+// =================================
+// GeoSpiirit - Cell Renderer
+// =================================
+
+
+let activeLayers=[];
+
+
+
+function renderCell(cell){
+
+
+    clearCellLayers();
+
+
+
+    // ROUTES
+
+    if(cell.roads){
+
+
+        cell.roads.forEach(road=>{
+
+
+            if(!road.discovered)
+                return;
+
+
+
+            let line=L.polyline(
+
+                road.points,
+
+                {
+                    color:"gray",
+                    weight:4
+                }
+
+            );
+
+
+            line.addTo(map);
+
+
+            activeLayers.push(line);
+
+
+        });
+
+
+    }
+
+
+
+
+
+    // LOCALITES
+
+    if(cell.places){
+
+
+        cell.places.forEach(place=>{
+
+
+            if(!place.discovered)
+                return;
+
+
+
+            let marker=L.marker(
+
+                [
+                    place.lat,
+                    place.lng
+                ]
+
+            );
+
+
+            marker.bindPopup(
+
+                place.name
+
+            );
+
+
+            marker.addTo(map);
+
+
+            activeLayers.push(marker);
+
+
+
+        });
+
+
+    }
+
+
+
+
+
+    // POI
+
+    if(cell.pois){
+
+
+        cell.pois.forEach(poi=>{
+
+
+            if(!poi.discovered)
+                return;
+
+
+
+            let marker=L.marker(
+
+                [
+                    poi.lat,
+                    poi.lng
+                ]
+
+            );
+
+
+            marker.bindPopup(
+
+                poi.name
+
+            );
+
+
+            marker.addTo(map);
+
+
+            activeLayers.push(marker);
+
+
+
+        });
+
+
+    }
+
+
+}
+
+
+
+function clearCellLayers(){
+
+
+    activeLayers.forEach(layer=>{
+
+
+        map.removeLayer(layer);
+
+
+    });
+
+
+    activeLayers=[];
+
+
+}
