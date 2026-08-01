b0c0c79684b4897f79c511d8d962339ac0ef43ce
ImNotYourSpiirit <jules2004.restelli@gmail.com>
2026-07-31 20:30:20 -0500

    Create buildChunks.js

---
+++ tools/buildChunks.js
@@ -0,0 +1,11 @@
+module.exports = async function(country){
+
+    console.log("Création des chunks...");
+
+    // TODO :
+    // Répartir les routes et les POI
+    // dans les différents chunks
+
+    console.log("Chunks terminés.");
+
+};
