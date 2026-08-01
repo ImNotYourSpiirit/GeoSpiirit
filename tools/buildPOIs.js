9d0270fdab090a0902ef0783634c1af1e0e5ba7b
ImNotYourSpiirit <jules2004.restelli@gmail.com>
2026-07-31 20:27:13 -0500

    Update buildPOIs.js

--- tools/buildPOIs.js
+++ tools/buildPOIs.js
@@ -1,1 +1,14 @@
-//
+module.exports = async function(country){
+
+    console.log("Création des POI...");
+
+    // TODO :
+    // Lire les données OSM
+    // Extraire les villes
+    // Extraire les villages
+    // Extraire les commerces
+    // Sauvegarder dans data/world/{country}/pois
+
+    console.log("POI terminés.");
+
+};
