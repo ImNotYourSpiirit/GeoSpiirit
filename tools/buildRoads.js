b59e720889c24d04cac9764a692e37d3c91bca60
ImNotYourSpiirit <jules2004.restelli@gmail.com>
2026-07-31 20:27:33 -0500

    Update buildRoads.js

--- tools/buildRoads.js
+++ tools/buildRoads.js
@@ -1,1 +1,12 @@
-//
+module.exports = async function(country){
+
+    console.log("Création des routes...");
+
+    // TODO :
+    // Lire les routes OpenStreetMap
+    // Les découper en segments
+    // Sauvegarder dans data/world/{country}/roads
+
+    console.log("Routes terminées.");
+
+};
